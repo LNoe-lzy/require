526e3a65e60e05dbc0362a00cfe9208815c5ce70
LNoe-lzy <15222505516@163.com>
2017-05-15 13:48:26 +0800

    增加module类

--- require.js
+++ require.js
@@ -5,10 +5,10 @@ let require, define;
         console.error('当前环境非浏览器环境。');
     }
 
-    let mid = 0;         // 模块标示id
-    let modules = {};    // 模块列表
-    let main;            // 主入口
-    let depMap = {};     // 映射列表
+    let mid = 0; // 模块标示id
+    let modules = {}; // 模块列表
+    let main; // 主入口
+    let depMap = {}; // 映射列表
 
     /**
      * 入口文件的加载函数
@@ -17,9 +17,14 @@ let require, define;
      * error: 错误处理函数
      */
     require = function (dep, callback, error) {
-
+        modules[main.name] = main;
+        Object.assign(main, {
+            dep,
+            callback,
+            error
+        });
     };
-    
+
     /**
      * 模块定义函数
      * name: 函数名称
@@ -28,6 +33,59 @@ let require, define;
      * error: 错误处理函数
      */
     define = function (name, dep, callback, error) {
-
+        let module = modules[name];
+        module.name = name;
+        module.dep = dep;
+        module.callback = callback;
+        module.error = error;
     };
+
+    class Module {
+        /**
+         * Module类构造函数
+         * @param {String} name 
+         * @param {Array} dep 
+         * @param {Function} callback 
+         * @param {Function} error 
+         * 
+         * @memberOf Module
+         */
+        constructor(name, dep, callback, error) {
+            this.mid = ++mid;
+            this.init(name, dep, callback, error);
+            this.create();
+        }
+
+        /**
+         * 模块初始化函数
+         * 
+         * @param {String} name 
+         * @param {Array} dep 
+         * @param {Function} callback 
+         * @param {Function} error 
+         * 
+         * @memberOf Module
+         */
+        init(name, dep, callback, error) {
+            this.name = name;
+            this.dep = dep;
+            this.callback = callback;
+            this.error = error;
+            this.src = `./${name}`;
+        }
+
+        /**
+         * 添加script节点
+         * 
+         * @memberOf Module
+         */
+        create() {
+            let script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = this.src;
+            document.body.appendChild(script);
+            script.onerror = this.error;
+
+        }
+    }
 })(this);
